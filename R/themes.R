91f8ce1f029be74352dca9622afdf6ed3a7d71d4
Merlin Scherer <scherer@kof.ethz.ch>
2025-04-16 10:33:59 +0200

    Reduces default bar gap

--- R/themes.R
+++ R/themes.R
@@ -567,7 +567,7 @@ init_tsggplot_theme <- function(
       ETH_Purple_60 = colors$ETH_Purple$`60`,
       ETH_Purple_40 = colors$ETH_Purple$`40`
     ),
-    bar_gap = 0.8,
+    bar_gap = 0.1,
     bar_group_gap = 30,
     ci_alpha = "44",
     ci_colors = line_colors,
